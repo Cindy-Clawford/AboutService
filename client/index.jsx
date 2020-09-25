be201a89457633cb925321367e004d416d5df6cf
kcmcminn <kcmcminn@gmail.com>
2020-09-24 21:33:22 -0500

    Set up npm scripts, app component and fixed gitignore file

---
+++ client/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor() {
+    super ();
+
+  }
+  render() {
+    return (
+      <div className="about-container">
+        About
+      </div>
+    )
+  }
+}
