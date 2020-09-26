39e0fbfeb76757a4554b3f2a14e06ac4a7c80041
kcmcminn <kcmcminn@gmail.com>
2020-09-25 21:58:35 -0500

    Created get request to query database for one hotel entry

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const Hotels = require('../database/Hotels.js');
 
 const app = express ();
 const port = 4001;
 
 app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('/hotel', (req, res) => {
+  Hotels.findOne()
+    .exec((err, result) => {
+      res.send(result.hotel_name)
+    })
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
