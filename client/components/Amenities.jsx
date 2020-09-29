9c053ac48b352a5ecb8f90e15705eeb8a2151dd7
kcmcminn <kcmcminn@gmail.com>
2020-09-28 20:16:15 -0500

    Added description component, fixed some typos in amenities and ratings files

--- client/components/Amenities.jsx
+++ client/components/Amenities.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Amenities = (props) => {
-  var amenities = []; var property_amenitiesValues = Object.values(props.hotel.property_amenities);
+  var amenities = [];
+  var property_amenitiesValues = Object.values(props.hotel.property_amenities);
   var property_amenitiesKeys = Object.keys(props.hotel.property_amenities);
   for (var i = 0; i < property_amenitiesKeys.length; i++) {
     if (property_amenitiesValues[i]) {
