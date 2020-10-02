d1a16a5ba9c18ed1581ddff71d7bf8462867694f
kcmcminn <kcmcminn@gmail.com>
2020-10-02 01:02:24 -0500

    Working on styling images and creating a full view button

--- client/components/Images.jsx
+++ client/components/Images.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const ImagesContainer = styled.div`
+  margin: 5px 0;
+`
+
+const LargeImageContainer = styled.div`
+  position: absolute;
+  width: 415px;
+`
+const LargeImage = styled.img`
+  width: 100%;
+  height: auto;
+`
+const ThumbnailContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-column-gap: 2px;
+`
+
+const ThumbnailPhoto = styled.img`
+  filter: grayscale(75%);
+`
+
+const FullViewButton = styled.button`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`
 const Images = (props) => {
 
   return (
-    <div>
-      <img src={props.hotel.images[0]} width="400" height="300"/>
-      <div>
+    <ImagesContainer>
+      <LargeImageContainer>
+        <LargeImage src={props.hotel.images[0]}/>
+        <FullViewButton>Full View</FullViewButton>
+      </LargeImageContainer>
+      <ThumbnailContainer>
         {props.hotel.images.map((image, index) => {
           return (
-          <img key={index} src={image} width="50" height="50"/>
+          <ThumbnailPhoto key={index} src={image} width="50" height="50"/>
           )
         })}
-      </div>
-    </div>
+      </ThumbnailContainer>
+    </ImagesContainer>
   )
 }
 
