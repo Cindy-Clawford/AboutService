3ee7753dd8f1b212e07122eda9d549b50c1cba90
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-14 20:20:37 -0700

    Prelim Postgres query structure and text added to API

--- server/postgres-query.js
+++ server/postgres-query.js
@@ -1,3 +1,5 @@
+
+
 const databaseMethods = require('../database/Hotels');
 
 // Writing this note to indicate that this page will reference the CRUD operations for PostgreSQL data.
@@ -5,50 +7,42 @@ const databaseMethods = require('../database/Hotels');
 
 function postgresGet(filter) {
   return new Promise(function(resolve, reject) {
-    databaseMethods.Hotels.findOne(filter)
-      .exec((err, result) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(result);
-      })
-    });
+    // comment here to indicate the database below needs to be properly named.
+    database
+      .query('SELECT * FROM hotels WHERE hotelId = $1', [filter.hotel_name])
+      .then(result => resolve(result))
+      .catch(err => reject(err.stack))
+  });
 }
 
 function postgresPost(newEntry) {
   return new Promise(function(resolve, reject) {
-  databaseMethods.Hotels.create(newEntry)
-    .exec((err, result) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(result);
-    })
-  })
+    // comment here to indicate the database below needs to be properly named and the data format for newEntry needs to be reformatted
+    database
+      .query('INSERT INTO hotels VALUES($1) RETURNING *', [newEntry])
+      .then(result => resolve(result))
+      .catch(err => reject(err.stack))
+  });
 }
 
 function postgresPut(filter, update) {
   return new Promise(function(resolve, reject) {
-  databaseMethods.Hotels.updateOne(filter, update)
-    .exec((err, result) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(result);
-    })
-  })
+    // comment here to indicate the database below needs to be properly named and the data format for update needs to be reformatted
+    database
+      .query('INSERT INTO hotels($1) VALUES($2) RETURNING *', [filtern.hotel_name, update])
+      .then(result => resolve(result))
+      .catch(err => reject(err.stack))
+  });
 }
 
 function postgresDelete(filter) {
   return new Promise(function(resolve, reject) {
-  databaseMethods.Hotels.deleteOne(filter)
-    .exec((err, result) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(result);
-    })
-  })
+    // comment here to indicate the database below needs to be properly named and the data format for update needs to be reformatted
+    database
+      .query('DELETE FROM hotels($1) RETURNING *', [filtern.hotel_name])
+      .then(result => resolve(result))
+      .catch(err => reject(err.stack))
+  });
 }
 
 module.exports.postgresGet = postgresGet;
