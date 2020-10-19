30686e6745b47eb9c71626caf5292960ece13c5f
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-19 15:25:00 -0700

    Query strings in postgres for bugs

--- database/postgreSQL/HotelsPostgres.js
+++ database/postgreSQL/HotelsPostgres.js
@@ -1,16 +1,29 @@
 const fs = require('fs')
-// const pg = require('pg')
-const Client = require('pg').Client
-const connectionString = 'postgresql://localhost:5433/hotel'
-const pg = new Client( { connectionString: connectionString } )
-
+const path = require('path')
+const { Pool, Client } = require('pg')
 const copyFrom = require('pg-copy-streams').from;
 
-pg.connect(function(err, client, done) {
-  var stream = client.query(copyFrom('COPY about FROM STDIN'));
-  var fileStream = fs.createReadStream('../../datafile.csv')
-  fileStream.on('error', done);
-  fileStream.pipe(stream).on('finish', done).on('error', done);
+const pool = new Pool({
+  host: 'localhost',
+  user: 'akilduff',
+  database: 'hotel'
+})
+
+var inputFile = path.join(__dirname, '../../datafile.csv')
+const queryString = `COPY public.about(hotel_name) FROM ${inputFile} CSV HEADER`
+const queryString2 = `SELECT * FROM about`
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack)
+  }
+  client.query(copyFrom(queryString2), (err, result) => {
+    release()
+    if (err) {
+      return console.error('Error executing query', err.stack)
+    }
+    console.log(result)
+  })
 });
 
 
