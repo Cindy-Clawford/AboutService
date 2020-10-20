aa02a2ec3fcdc0c5d177d933fbb5af6f1016fcbb
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-20 11:38:31 -0700

    Generated and seeded 10m entries to postgres

--- database/dataGeneration.js
+++ database/dataGeneration.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const faker = require('faker');
 const descriptionGenerator = require('./descriptionGenerator.js');
-const numOfRecords = 1000;
+const numOfRecords = 10000000;
 
 const dataTitles = `hotel_name,_description,overall_rating,number_of_reviews,rank,location_ranking,cleanliness_rating,service_rating,value_rating,"Valet parking","Swimming Pool","Free breakfast","Beach","Babysitting","Free internet","Fitness center","Entertainment","Business center","Spa","Diving","Wifi","Hot tub","Kids club","Fishing","Airport transportation","Banquet room","Couples massage","Taxi service","Steam room","Salon","Gift shop","ATM on site","Dry cleaning","allday front desk","Karaoke","Aerobics","Swimup bar","Snack bar","Meeting rooms","Tennis courts","Free parking","Breakfast buffet","Shuttle bus service","allday security","Concierge","Currency exchange","Non-smoking hotel","Sun loungers_beach chairs","Doorperson","Shops","Air conditioning","Room service","Flatscreen TV","Safe","Wake-up service","Housekeeping","Iron","Balcony","Private beach","Additional bathroom","Interconnected rooms available","Kitchenette","Laptop safe","VIP room facilities","Refrigerator","Private balcony","Sofa","DVD_CD player","Microwave","Ocean View","Partial Ocean View","Suites","Family rooms","Non-smoking rooms",hotel_class,hotel_style,hotel_website,images,languages_spoken`
 
--- server/postgres-query.js
+++ server/postgres-query.js
@@ -5,7 +5,7 @@ function postgresGet(filter) {
   return new Promise(function(resolve, reject) {
     // comment here to indicate the database below needs to be properly named.
     database
-      .query('SELECT * FROM hotels WHERE hotelId = $1', [filter.hotel_name])
+      .query('SELECT * FROM about WHERE hotelId = $1', [filter.hotel_name])
       .then(result => resolve(result))
       .catch(err => reject(err.stack))
   });
@@ -15,7 +15,7 @@ function postgresPost(newEntry) {
   return new Promise(function(resolve, reject) {
     // comment here to indicate the database below needs to be properly named and the data format for newEntry needs to be reformatted
     database
-      .query('INSERT INTO hotels VALUES($1) RETURNING *', [newEntry])
+      .query('INSERT INTO about VALUES($1) RETURNING *', [newEntry])
       .then(result => resolve(result))
       .catch(err => reject(err.stack))
   });
@@ -25,7 +25,7 @@ function postgresPut(filter, update) {
   return new Promise(function(resolve, reject) {
     // comment here to indicate the database below needs to be properly named and the data format for update needs to be reformatted
     database
-      .query('INSERT INTO hotels($1) VALUES($2) RETURNING *', [filtern.hotel_name, update])
+      .query('INSERT INTO about($1) VALUES($2) RETURNING *', [filtern.hotel_name, update])
       .then(result => resolve(result))
       .catch(err => reject(err.stack))
   });
@@ -35,7 +35,7 @@ function postgresDelete(filter) {
   return new Promise(function(resolve, reject) {
     // comment here to indicate the database below needs to be properly named and the data format for update needs to be reformatted
     database
-      .query('DELETE FROM hotels($1) RETURNING *', [filtern.hotel_name])
+      .query('DELETE FROM about($1) RETURNING *', [filtern.hotel_name])
       .then(result => resolve(result))
       .catch(err => reject(err.stack))
   });
