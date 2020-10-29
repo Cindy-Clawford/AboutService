623956328eda7e3bf0225c74294ff214b81d9aff
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-10-29 16:43:30 -0700

    Include compression

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require("path");
 const webpack = require("webpack");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
+  plugins: [new CompressionPlugin({
+    test: /\.js(\?.*)?$/i,
+    algorithm: 'gzip',
+    minRatio: 0.8,
+  })],
   entry: path.join(__dirname + "/client/index.jsx"),
   mode: "development",
   module: {
